059d6fd00aa02828215863a5ad1f50743e9cf50e
Karlduino <karlduino@gmail.com>
2014-01-04 08:20:35 -0600

    expand RouterReset_Yun to include reset of the router

--- RouterReset_Yun/RouterReset_Yun.ino
+++ RouterReset_Yun/RouterReset_Yun.ino
@@ -3,20 +3,31 @@
  * red LED (internet down): pin 3
  * green LED (internet up): pin 5
  * yellow LED (testing):    pin 6
+ * 
  *
  * needs checkInternet.py installed in /usr/bin in Yun Linino
  */
 
 #include <Process.h>
 
-const int redLED = 3;
-const int greenLED = 5;
-const int yellowLED = 6;
+const int redLED     = 3;
+const int greenLED   = 5;
+const int yellowLED  = 6;
+const int routerPin1 = 8;
+const int routerPin2 = 9;
+const int resetRouterTime = 30000;
+const int timeBetweenPings = 5000;
+
+String internetStatus;
 
 void setup() {
     pinMode(redLED, OUTPUT);
     pinMode(greenLED, OUTPUT);
     pinMode(yellowLED, OUTPUT);
+    pinMode(routerPin1, OUTPUT);
+    pinMode(routerPin2, OUTPUT);
+    digitalWrite(routerPin1, HIGH);
+    digitalWrite(routerPin2, HIGH);
 
     Bridge.begin();  // make contact with the linux processor
 
@@ -35,28 +46,35 @@ void loop() {
     digitalWrite(yellowLED, HIGH);
     internetCheck.runShellCommand("/usr/bin/checkInternet.py");
 
-    while(internetCheck.available() > 0)
-    {
-        char c = internetCheck.read();
+    internetStatus = "";
+    while(internetCheck.available() > 0) {
+      char c = internetCheck.read();
+      if(c != '\n') internetStatus += c;
+    }
 
-        if(c == '1')
-        {
-            digitalWrite(redLED, LOW);
-            digitalWrite(greenLED, HIGH);
-            break;
-        }
-        else if(c == '0')
-        {
-            digitalWrite(redLED, HIGH);
-            digitalWrite(greenLED, LOW);
-            break;
-        }
+    if(internetStatus[0] == '1')
+    {
+        digitalWrite(redLED, LOW);
+        digitalWrite(greenLED, HIGH);
+    }
+    else if(internetStatus[0] == '0')
+    {
+        digitalWrite(redLED, HIGH);
+        digitalWrite(greenLED, LOW);
     }
     
-    while(internetCheck.available() > 0) // read everything else
-        internetCheck.read();
-
     digitalWrite(yellowLED, LOW);
 
-    delay(5000);
+    if(internetStatus[0] == '0')
+        resetRouter();
+    else
+        delay(timeBetweenPings);
+}
+
+void resetRouter(void) {
+  digitalWrite(routerPin1, LOW);
+  digitalWrite(routerPin2, LOW);
+  delay(resetRouterTime);
+  digitalWrite(routerPin1, HIGH);
+  digitalWrite(routerPin2, HIGH);
 }
